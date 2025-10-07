51d56c92acd0387dfbdfc096eb0cf0d10a2f61d0
khanh hoang <khankga012@gmail.com>
2025-10-07 20:31:07 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import axios from "./utils/axios.customize"
+import { useEffect } from "react"
+
+function App() {
+  useEffect(() => {
+    const fetchHelloWorld = async () => {
+      const res = await axios.get('/v1/api');
+      console.log(">>> check res:", res);
+    };
+    fetchHelloWorld();
+  }, []);
+
+  return <>hello world</>;
+}
+
+export default App;
